d36d26ffc2ac37fd40de43b1fd5cf1aeff81660c
Hüseyin Atak <huseyin.atak@outlook.com.tr>
2022-10-18 16:35:01 +0300

    Kutuphaneler Eklendi. Componentler Olusturuldu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { AnimalComponent } from './animals/animal/animal.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoryComponent,
+    AnimalsComponent,
+    AnimalComponent,
+    AnimalDetailComponent
   ],
   imports: [
     BrowserModule,
